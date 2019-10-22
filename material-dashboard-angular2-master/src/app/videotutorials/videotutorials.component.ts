b637e7913cb877d750aa79a67d569b40f3143699
Sbhavyarao <bhavyasrirao@gmail.com>
2019-10-22 13:28:32 -0500

    dashboard

---
+++ material-dashboard-angular2-master/src/app/videotutorials/videotutorials.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-videotutorials',
+  templateUrl: './videotutorials.component.html',
+  styleUrls: ['./videotutorials.component.scss']
+})
+export class VideotutorialsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
